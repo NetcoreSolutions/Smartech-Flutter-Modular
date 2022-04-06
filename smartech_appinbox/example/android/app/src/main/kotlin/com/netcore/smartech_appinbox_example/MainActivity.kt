b34c3a3fbcd7d4a27573b0989413b27c94c993a3
Nitesh Chouhan <nitesh.chouhan@systematixindia.com>
2022-04-06 17:28:53 +0530

    Remove the extra example directory in the plugins

--- smartech_appinbox/example/android/app/src/main/kotlin/com/netcore/smartech_appinbox_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.netcore.smartech_appinbox_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- smartech_base/example/android/app/src/main/kotlin/com/netcore/smartech_base_example/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.netcore.smartech_base_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
