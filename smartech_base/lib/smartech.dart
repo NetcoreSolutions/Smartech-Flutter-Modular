b9ba921058e7149c390f6b04e2339d560dca1c37
Nitesh Chouhan <nitesh.chouhan@systematixindia.com>
2022-04-07 17:49:12 +0530

    smartech_base -> lib -> smartech - add DebugLevel class - add custom type defined

--- smartech_base/lib/smartech.dart
+++ smartech_base/lib/smartech.dart
@@ -4,12 +4,12 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class Smartech {
+
   static const MethodChannel _channel = MethodChannel('smartech_base_channel');
 
+  //To make singleton class
   static final Smartech _smartech = Smartech._internal();
-
   factory Smartech() => _smartech;
-
   Smartech._internal() {
   }
 
@@ -19,3 +19,18 @@ class Smartech {
   }
 
 }
+
+class DebugLevel {
+  static const int verbose = 1;
+  static const int debug = 2;
+  static const int info = 3;
+  static const int warn = 4;
+  static const int error = 5;
+  static const int fatal = 6;
+  static const int none = 7;
+}
+
+//custom type defined
+typedef CustomHTMLCallback = Future<dynamic> Function(Map<String, dynamic>? payload);
+typedef OnhandleDeeplinkAction = Function(String? deeplinkigUrl,Map<dynamic, dynamic>? payload, bool? isAfterTerminated);
+
