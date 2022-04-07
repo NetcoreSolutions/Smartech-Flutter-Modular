e414e42d0b09cb07c5772fed9f827a67bfe11da3
Nitesh Chouhan <nitesh.chouhan@systematixindia.com>
2022-04-07 16:10:47 +0530

    smartech_base -> android -> SmartechBasePlugin - add bridging methods to the onMethodCall

--- smartech_base/android/src/main/kotlin/com/netcore/smartech_base/SmartechBasePlugin.kt
+++ smartech_base/android/src/main/kotlin/com/netcore/smartech_base/SmartechBasePlugin.kt
@@ -9,6 +9,7 @@ import android.location.LocationManager
 import android.util.Log
 import androidx.annotation.NonNull
 import com.netcore.android.Smartech
+import com.netcore.android.inapp.InAppCustomHTMLListener
 import io.flutter.embedding.android.FlutterActivity
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -35,10 +36,163 @@ class SmartechBasePlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+
+    when(call.method){
+      "getPlatformVersion" -> {
+        result.success("Android ${android.os.Build.VERSION.RELEASE}")
+      }
+      "setDebugLevel" -> {
+        setDebugLevel(call.arguments as Int)
+        result.success(null)
+      }
+      "trackAppInstall" -> {
+        trackAppInstall()
+        result.success(null)
+      }
+      "trackAppUpdate" -> {
+        trackAppUpdate()
+        result.success(null)
+      }
+      "trackAppInstallUpdateBySmartech" -> {
+        trackAppInstallUpdateBySmartech()
+        result.success(null)
+      }
+      "setInAppCustomHTMLListener" -> {
+        val callback = object : InAppCustomHTMLListener {
+          override fun customHTMLCallback(payload: HashMap<String, Any>?) {
+            runOnMainThread(Runnable {
+              channel.invokeMethod("setInAppCustomHTMLListener", payload)
+            })
+          }
+        }
+        smartech.setInAppCustomHTMLListener(callback)
+      }
+      "updateUserProfile" -> {
+        updateUserProfile(call.arguments as HashMap<String, Any>)
+        result.success(null)
+      }
+      "setUserIdentity" -> {
+        setUserIdentity(call.arguments as String)
+        result.success(null)
+      }
+      "login" -> {
+        login(call.arguments as String)
+        result.success(null)
+      }
+      "clearUserIdentity" -> {
+        smartech.clearUserIdentity()
+        result.success(null)
+      }
+      "logoutAndClearUserIdentity" -> {
+        smartech.logoutAndClearUserIdentity(call.arguments as Boolean)
+        result.success(null)
+      }
+      "trackEvent" -> {
+        trackEvent(call.arguments as HashMap<String, Any>)
+        result.success(null)
+      }
+      "getDeviceUniqueId" -> {
+        result.success(smartech.getDeviceUniqueId())
+        result.success(null)
+      }
+      "setUserLocation" -> {
+        setUserLocation(call.arguments as HashMap<String, Any>)
+        result.success(null)
+      }
+      "optTracking" -> {
+        smartech.optTracking(call.arguments as Boolean)
+        result.success(null)
+      }
+      "hasOptedTracking" -> {
+        result.success(smartech.hasOptedTracking())
+      }
+      "optInAppMessage" -> {
+        smartech.optInAppMessage(call.arguments as Boolean)
+        result.success(null)
+      }
+      "hasOptedInAppMessage" -> {
+        result.success(smartech.hasOptedInAppMessage())
+      }
+      "processEventsManually" -> {
+        smartech.processEventsManually()
+        result.success(null)
+      }
+      "getUserIdentity" -> {
+        result.success(smartech.getUserIdentity())
+      }
+      "openNativeWebView" -> {
+        openNativeWebView?.invoke()
+        result.success(null)
+      }
+      "onHandleDeeplinkAction" -> {
+        SmartechDeeplinkReceivers.deeplinkReceiverCallBack = { deepLinkUrl, payload ->
+          if (deepLinkUrl != null) {
+            var map = HashMap<String, Any>()
+
+            map.put("deeplinkURL", deepLinkUrl)
+
+            if (payload != null) {
+              map.put("customPayload", payload)
+            }
+            Log.d("onReceive", "onReceive onInvoke post")
+            Log.d("onReceive", map.toString())
+
+            context.getSharedPreferences("Deeplink_action", Context.MODE_PRIVATE).edit().putString("deepLinkUrl", deepLinkUrl).putString("payload", payload).apply()
+
+            runOnMainThread(Runnable {
+              channel.invokeMethod("onhandleDeeplinkAction", map, object : Result {
+                override fun success(result: Any?) {
+                  context.getSharedPreferences("Deeplink_action", Context.MODE_PRIVATE).edit().clear().apply()
+                  Log.d("success", "success")
+                }
+
+                override fun error(code: String?, msg: String?, details: Any?) {
+                  if (msg != null) {
+                    Log.d("success111", msg)
+                  }
+                }
+
+                override fun notImplemented() {
+                  Log.d("notImplemented", "notImplemented")
+                }
+              })
+            })
+          }
+        }
+      }
+      "onHandleDeeplinkActionBackground" -> {
+        val deeplinkUrl = context.getSharedPreferences("Deeplink_action", Context.MODE_PRIVATE).getString("deepLinkUrl", null)
+        val payload = context.getSharedPreferences("Deeplink_action", Context.MODE_PRIVATE).getString("payload", null)
+        if (deeplinkUrl != null) {
+          Log.d("deeplinkUrl", deeplinkUrl)
+          var map = HashMap<String, Any>()
+          map.put("deeplinkURL", deeplinkUrl)
+          if (payload != null) {
+            map.put("customPayload", payload)
+          }
+          runOnMainThread(Runnable {
+            channel.invokeMethod("onhandleDeeplinkAction", map, object : Result {
+              override fun success(result: Any?) {
+                context.getSharedPreferences("Deeplink_action", Context.MODE_PRIVATE).edit().clear().apply()
+                Log.d("success", "success")
+              }
+              override fun error(code: String?, msg: String?, details: Any?) {
+                if (msg != null) {
+                  Log.d("success111", msg)
+                }
+              }
+              override fun notImplemented() {
+                Log.d("notImplemented", "notImplemented")
+              }
+            })
+          })
+        }
+      }
+      "openUrl" -> {
+        openUrl?.invoke(call.arguments as String)
+      }
+      else -> result.notImplemented()
+
     }
   }
 
