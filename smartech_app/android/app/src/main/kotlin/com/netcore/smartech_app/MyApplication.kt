3c0ae45146ff9bc2ebca595744fcd46ae01e0c13
Nitesh Chouhan <nitesh.chouhan@systematixindia.com>
2022-04-11 19:09:03 +0530

    smartech_app -> android -> MyApplication.kt -initialize SmartechPushPlugin and add notification listener -extends SMTNotificationListener class -oevrride getSmartechNotifications method

--- smartech_app/android/app/src/main/kotlin/com/netcore/smartech_app/MyApplication.kt
+++ smartech_app/android/app/src/main/kotlin/com/netcore/smartech_app/MyApplication.kt
@@ -2,11 +2,14 @@ package com.netcore.smartech_app
 
 import android.util.Log
 import com.netcore.android.Smartech
+import com.netcore.android.smartechpush.SmartPush
+import com.netcore.android.smartechpush.notification.SMTNotificationListener
 import com.netcore.smartech_base.SmartechBasePlugin
+import com.netcore.smartech_push.SmartechPushPlugin
 import io.flutter.app.FlutterApplication
 import java.lang.ref.WeakReference
 
-class MyApplication: FlutterApplication() {
+class MyApplication: FlutterApplication(), SMTNotificationListener {
     override fun onCreate() {
         super.onCreate()
         Log.d("MyApplication","onCreate")
@@ -16,13 +19,24 @@ class MyApplication: FlutterApplication() {
         Smartech.getInstance(WeakReference(applicationContext)).setDebugLevel(9)
         Smartech.getInstance(WeakReference(applicationContext)).trackAppInstallUpdateBySmartech()
 
-        //Initialize Smartech Flutter Base Plugin
+        //Initialize Flutter Smartech Base Plugin
         SmartechBasePlugin.initializePlugin(this)
+
+        //Initialize Flutter Smartech Push Plugin
+        SmartechPushPlugin.initializePlugin(this)
+
+        //Add SmartPush Notification Listener
+        SmartPush.getInstance(WeakReference(SmartechPushPlugin.context)).setSMTNotificationListener(this)
 
     }
 
     override fun onTerminate() {
         super.onTerminate()
         Log.d("onTerminate","onTerminate")
+    }
+
+    //implement override Smartech Notification method
+    override fun getSmartechNotifications(payload: String, source: Int) {
+        SmartPush.getInstance(WeakReference(SmartechPushPlugin.context)).renderNotification(payload, source)
     }
 }
